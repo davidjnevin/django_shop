7f0129cfef6e828a881c762f8d36b689a682c646
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2022-10-06 17:54:33 +0200

    Create cart.

--- myshop/settings.py
+++ myshop/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "shop.apps.ShopConfig",
+    "cart.apps.CartConfig",
 ]
 
 MIDDLEWARE = [
@@ -117,3 +118,6 @@ MEDIA_ROOT = BASE_DIR / "media"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+CART_SESSION_ID = "cart"
