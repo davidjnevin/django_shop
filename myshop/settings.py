c61a29b33299e0c876f1ccb66aa49e7eba5ce120
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2022-10-06 12:43:41 +0200

    Added Product and Category Models and templates

--- myshop/settings.py
+++ myshop/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "shop.apps.ShopConfig",
 ]
 
 MIDDLEWARE = [
@@ -110,6 +111,8 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR / "media"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
