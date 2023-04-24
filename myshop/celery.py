9531fa72877ad4eeb1b90469e71715cc0d876e28
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-04-24 17:38:06 +0200

    Remove app.task and lamnda: settings.INSTALLED_APPS

--- myshop/celery.py
+++ myshop/celery.py
@@ -1,16 +1,13 @@
+import os
 from celery import Celery
 from django.conf import settings
 
 # set the default Django settings module for the 'celery' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myshop.settings.dev')
 app = Celery("myshop")
 
 # Using a string here means the worker will not have to
 # pickle the object when using Windows.
 app.config_from_object("django.conf:settings", namespace="CELERY")
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
-
-
-@app.task(bind=True)
-def debug_task(self):
-    print(f"Request: {self.request!r}")
+app.autodiscover_tasks()
 
