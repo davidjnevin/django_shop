7b1d61a57221602f6b40f317d1db995dbbfea55d
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-04-25 11:13:28 +0200

    Prep for prod

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,23 +3,6 @@
 set -e
 set -o nounset
 
-postgres_ready() {
-    python << END
-import sys
-
-from psycopg2 import connect
-from psycopg2.errors import OperationalError
-
-try:
-    connect(
-        dbname="${POSTGRES_DB}",
-        user="${POSTGRES_USER}",
-        password="${POSTGRES_PASSWORD}",
-    )
-except OperationalError:
-    sys.exit(-1)
-END
-}
 
 redis_ready() {
     python << END
@@ -33,6 +16,24 @@ try:
     redis = Redis.from_url("${CELERY_BROKER_URL}", db=0)
     redis.ping()
 except RedisError:
+    sys.exit(-1)
+END
+}
+
+postgres_ready() {
+    python << END
+import sys
+
+from psycopg2 import connect
+from psycopg2.errors import OperationalError
+
+try:
+    connect(
+        dbname="myshop",
+        user="davidjnevin",
+        password="Password10!",
+    )
+except OperationalError:
     sys.exit(-1)
 END
 }
